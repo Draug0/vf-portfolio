446f49f7d7b95711015a174c5f1fc02e0df90b28
Draug0 <85965310+Draug0@users.noreply.github.com>
2022-09-21 01:43:39 -0300

    About me & info changes

--- src/components/Organism/AboutMe/AboutMe.js
+++ src/components/Organism/AboutMe/AboutMe.js
@@ -1,13 +1,4 @@
-import { TitleSection, BoldSpan } from "../../../styled-component/styles";
-import {
-  AboutMeContainer,
-  AboutMePhoto,
-  AboutMeContainerContent,
-  AboutMeParagraph,
-  AboutMeParagraphContainer,
-} from "./AboutMeStyles";
-import Image from "next/image";
-import Photo from "../../../images/VictoriaPhoto2.jpeg";
+import { Controller, Scene } from "react-scrollmagic";
 import styles from "./aboutme.module.scss";
 
 const AboutMe = () => {
@@ -15,36 +6,47 @@ const AboutMe = () => {
     <section className={styles.wrapper} id="aboutme">
       <article className="container">
         <h1>SOBRE MI</h1>
-        <div className={styles.contentContainer}>
-          <div className={styles.imgWrapper}>
-            <Image src={Photo} alt="Imagen" />
-          </div>
-          <div className={styles.textContainer}>
-            <p>
-              Hola, mi nombre es Victoria Fernandez y estudié Community Manager
-              & Publicidad en Coderhouse.
-            </p>
-            <p>
-              Actualmente me sigo formando en marketing digital porque me
-              apasiona!
-            </p>
-            <p>Nuestro trabajo es gestionar y manejar las redes sociales.</p>
-            <p>
-              Lograremos llevar tu emprendimiento a otro nivel mediante
-              estrategias de marketing, creando relaciones con los clientes y
-              humanizando la marca.
-            </p>
-            <p>
-              Con el fin de generar comunidad, atraer clientes potenciales y así
-              aumentar las ventas de tu producto/servicio.
-            </p>
-            <p>
-              <strong>
-                ¿Preparado/a para llevar tu marca al siguiente nivel?
-              </strong>
-            </p>
-          </div>
-        </div>
+        <Controller>
+          <Scene
+            triggerElement={`.${styles.contentContainer}`}
+            triggerHook={0.6}
+            reverse={true}
+            classToggle={`fade-in`}
+          >
+            <div className={styles.contentContainer}>
+              <div className={styles.imgWrapper} />
+              <div className={styles.textContainer}>
+                <p>
+                  Hola, mi nombre es <strong>Victoria Fernandez</strong> y
+                  estudié Community Manager & Publicidad en Coderhouse.
+                </p>
+                <p>
+                  Actualmente me sigo formando en marketing digital porque me
+                  apasiona!
+                </p>
+                <p>
+                  <strong>
+                    Nuestro trabajo es gestionar y manejar las redes sociales.
+                  </strong>
+                </p>
+                <p>
+                  Lograremos llevar tu emprendimiento a otro nivel mediante
+                  estrategias de marketing, creando relaciones con los clientes
+                  y humanizando la marca.
+                </p>
+                <p>
+                  Con el fin de generar comunidad, atraer clientes potenciales y
+                  así aumentar las ventas de tu producto/servicio.
+                </p>
+                <p>
+                  <strong>
+                    ¿Preparado/a para llevar tu marca al siguiente nivel?
+                  </strong>
+                </p>
+              </div>
+            </div>
+          </Scene>
+        </Controller>
       </article>
     </section>
   );
